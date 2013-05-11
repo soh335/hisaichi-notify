1ee5dead0df23e3a80db9f80356961ac6419042f
soh335 <sugarbabe335@gmail.com>
2013-05-11 14:38:26 +0900

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HisaichiNotify::Application.config.secret_key_base = 'c6ba6b37122873ddee3e0eb95710b78e852365ba5bd878888af8f290fe00d0431e75a307ae70f4ea86d93b4a1d50cb3e1fdd29f9085d55fb6165645fef3ec630'
