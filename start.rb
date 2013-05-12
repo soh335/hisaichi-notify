8dd0679c883242b8afb8bbe9df8e5380ac821b9e
soh335 <sugarbabe335@gmail.com>
2013-05-12 10:40:02 +0900

    stdout.sync in config/development.rb

--- start.rb
+++ start.rb
@@ -1,8 +1,6 @@
 require ::File.expand_path('../config/environment',  __FILE__)
 require 'bundler/setup'
 
-$stdout.sync = true
-
 EM.run do
   Message.restore
   Thin::Server.start Rails.application, '0.0.0.0', ENV["PORT"]
