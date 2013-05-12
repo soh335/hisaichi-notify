a001ba0cfafece66689e274da0c87af740f5728a
soh335 <sugarbabe335@gmail.com>
2013-05-12 22:40:44 +0900

    redis ping at initialized

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,4 +3,6 @@ require 'uri'
 if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   $redis = Redis.new( :host => uri.host, :port => uri.port, :password => uri.password )
+  # http://memo.yomukaku.net/entries/331
+  $redis.ping
 end
