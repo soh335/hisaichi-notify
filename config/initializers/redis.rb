1daddc084eb4d466a448d123c1ab5ce27b6f8c2d
soh335 <sugarbabe335@gmail.com>
2013-05-12 11:53:29 +0900

    initializers/redis.rb で REDISTOGO_URL がない場合は実行しない

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 require 'uri'
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-
-$redis = Redis.new( :host => uri.host, :port => uri.port, :password => uri.password )
+if ENV["REDISTOGO_URL"]
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new( :host => uri.host, :port => uri.port, :password => uri.password )
+end
