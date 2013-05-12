c3b1fc9df9584c479ab463901d37e723f93f5bfc
soh335 <sugarbabe335@gmail.com>
2013-05-12 22:58:29 +0900

    rails_env が test じゃない場合は redis に ping する

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,5 +4,5 @@ if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
   $redis = Redis.new( :host => uri.host, :port => uri.port, :password => uri.password )
   # http://memo.yomukaku.net/entries/331
-  $redis.ping
+  $redis.ping if ENV["RAILS_ENV"] != "test"
 end
